f33f05780a7a8922ab264bfdd6eaf77d4f6b2b59
Stephen Watkins <swatkins@salsify.com>
2019-01-30 14:24:24 -0500

    specs and validation

--- lib/gtin/gtin.rb
+++ lib/gtin/gtin.rb
@@ -3,19 +3,24 @@ module GTIN
 
   GTIN_COMPATIBLE_ID_TYPES = %w(UPC GTIN EAN ISSN ISBN).freeze
 
+  GtinValidationError = Class.new(StandardError)
+
+  # raises GtinValidationError if the provided value is a GTIN variant with an invalid checksum.
+  # ISSN, ISBN-10, and UPC-E checksums are not validated.
   def to_gtin(id_type, value)
     zero_pad(standardize(id_type, value))
   end
 
   def standardize(id_type, value)
     case id_type.upcase
-    when 'GTIN', 'EAN'
+    when 'GTIN', 'EAN', 'UPC-A', 'ISBN-13'
+      fail_on_invalid_checksum(value)
       value
-    when 'UPC', 'UPC-A', 'UPC-E'
+    when 'UPC', 'UPC-E'
       standardize_upc(value)
     when 'ISSN'
       standardize_issn(value)
-    when 'ISBN'
+    when 'ISBN', 'ISBN-10'
       standardize_isbn(value)
     end
   end
@@ -23,6 +28,7 @@ module GTIN
   def standardize_upc(upc)
     case upc.length
     when 12
+      fail_on_invalid_checksum(upc)
       upc
     when 7
       # http://www.taltech.com/barcodesoftware/symbologies/upc
@@ -44,6 +50,7 @@ module GTIN
   def standardize_issn(issn)
     case issn.length
     when 13
+      fail_on_invalid_checksum(issn)
       issn
     when 8
       with_checksum('977' + issn[0..-2] + '00')
@@ -55,6 +62,7 @@ module GTIN
   def standardize_isbn(isbn)
     case isbn.length
     when 13
+      fail_on_invalid_checksum(isbn)
       isbn
     when 10
       with_checksum('978' + isbn[0..-2])
@@ -67,9 +75,8 @@ module GTIN
     gtin.rjust(14, '0')
   end
 
-
   # Checksum calculations are only compatible with standardized GTIN variants
-  # Excluded: ISSN, ISBN-10, UPC-E
+  # Excluded: ISSN, ISBN-10, UPC-E, EAN-8
 
   def with_checksum(unchecked)
     unchecked + compute_checksum(unchecked)
@@ -83,5 +90,13 @@ module GTIN
 
   def valid_checksum?(identifier)
     compute_checksum(identifier[0..-2]) == identifier[-1]
+  end
+
+  def fail_on_invalid_checksum(identifier)
+    expected_checksum = compute_checksum(identifier[0..-2])
+    actual_checksum = identifier[-1]
+    return if actual_checksum == expected_checksum
+
+    raise GtinValidationError.new("Identifier #{identifier} has invalid checksum #{actual_checksum} -- expected #{expected_checksum}")
   end
 end
